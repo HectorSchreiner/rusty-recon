6365f252f208d1eed71e4dfed2f10dbbac6bab4d
Hector <agenthector007@gmail.com>
2024-07-05 12:44:14 +0200

    post requests

--- src/http_client.rs
+++ src/http_client.rs
@@ -3,9 +3,15 @@ use reqwest::{Error, IntoUrl, Url};
 pub async fn get_request(url: impl IntoUrl) -> Result<(), Error> {
     let response = reqwest::get(url).await?;
     println!("Status: {}", response.status());
-    
+
     let body = response.text().await?;
     println!("Body:\n{}", body);
 
+    Ok(())
+}
+
+pub async fn post_request(url: impl IntoUrl) -> Result<(), Error> {
+    let client = reqwest::Client::new();
+    
     Ok(())
 }
